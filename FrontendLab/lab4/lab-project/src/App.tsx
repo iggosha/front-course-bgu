648d7ae328b95661aa231e73fc01d26f71395782
iggosha <iegolovkov@gmail.com>
2024-03-12 03:33:30 +0300

    Создана форма добавления товара, добавляется в массив в App.tsx

--- FrontendLab/lab4/lab-project/src/App.tsx
+++ FrontendLab/lab4/lab-project/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from "react";
+import AddPizzaForm from "./components/AddPizzaForm";
+import "./App.css";
+import Pizza from "./models/Pizza";
 
-function App() {
+const App: FC = () => {
+  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+  const addPizza = (newPizza: Pizza) => {
+    setPizzasList([...pizzasList, newPizza]);
+  };
+
+  console.log("pizzasList", pizzasList)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <span className="heading">ПиццаВайб</span>
+      <AddPizzaForm addPizza={addPizza}></AddPizzaForm>
     </div>
   );
-}
+};
 
 export default App;
